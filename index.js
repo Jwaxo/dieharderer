468f5189597956767c85c17cb9a8486044eb5659
Jwaxo <jeff@jeffwax.com>
2014-04-15 13:16:33 -0700

    Updated hard case to trigger better. Added -er case

--- index.js
+++ index.js
@@ -2,19 +2,24 @@ module.exports = function (string) {
   var match_name
     , movie_name
 
-  var harder_pattern = /([A-Z]|[a-z]+)[\s-][H|h][ard]/ // Name Hard/hard
+  var harder_pattern = /([A-Z]|[a-z]+)[\s-][H|h](?:ard)/ // Name Hard/hard
     , two_name_pattern = /([A-Z][a-z]+)[\s-]([A-Z][a-z]+)/ // Name Word
+    , er_pattern = /(\w+)[\s-]([A-Za-z]+)(?:er)/ // Name Worder
     , one_name_pattern = /\s([A-Z][a-z]+)/ // Word
     , toos_pattern = /\s(\w+)(?:(?:\,\s)|\s)(?:(?:\btoo\b)|(?:\btwo\b)|2)/ // Word two/too/2
     
   // Couldn't get this to work with a switch, stupidly. No idea what's up with that.
   var harder_match = harder_pattern.exec(string)
+    , er_match = er_pattern.exec(string)
     , two_name_match = two_name_pattern.exec(string)
     , one_name_match = one_name_pattern.exec(string)
     , toos_match = toos_pattern.exec(string)
     
   if (harder_match) {
-    movie_name = harderify(harder_match[1],'Hard')
+    movie_name = harderify(harder_match[1], 'Hard')
+  }
+  else if (er_match) {
+    movie_name = harderify(er_match[1], er_match[2])
   }
   else if (two_name_match) {
     movie_name = pick_style(two_name_match)
--- test.js
+++ test.js
@@ -1,6 +1,8 @@
 var comma_too = "I want to come, too."
   , no_comma_too = "There are bats too!"
   , hard = "This is going to suck hard."
+  , will_h = "I will have something else." // should return "undefined"
+  , word_harder = "We all need to work better!"
   , too = "I am coming too."
   , two = "There will be two of us."
   , name = "But what about Jeff?"
@@ -11,6 +13,8 @@ var find_sequel = require('./index.js')
 console.log(find_sequel(comma_too))
 console.log(find_sequel(no_comma_too))
 console.log(find_sequel(hard))
+console.log(find_sequel(will_h))
+console.log(find_sequel(word_harder))
 console.log(find_sequel(too))
 console.log(find_sequel(two))
 console.log(find_sequel(name))
